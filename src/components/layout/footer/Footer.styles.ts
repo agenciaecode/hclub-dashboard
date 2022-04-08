b56b002f12e469c99c5fcacada508b9d811cf01d
extendslcc <lucas.lcc@hotmail.com>
2022-04-08 18:17:42 -0300

    feat(components): implements self-contained default Footer component

---
+++ src/components/layout/footer/Footer.styles.ts
@@ -0,0 +1,13 @@
+import { styled } from '@/theme';
+
+const StyledFooter = styled('footer', {
+  color: '$textBlack',
+  backgroundColor: '$bgWhite',
+  fontSize: '$base',
+  lineHeight: '$base',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export { StyledFooter };
