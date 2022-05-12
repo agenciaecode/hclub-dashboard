ce7395dbfd52a71e6ab1040b46e04feb160007ff
extendslcc <lucas.lcc@hotmail.com>
2022-05-12 15:10:21 -0300

    feat(utils): add extensible typescript exception class utility

---
+++ src/utils/error/exception/Exception.ts
@@ -0,0 +1,21 @@
+/**
+ * Custom Error class to be extended by all custom exceptions
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#examples}
+ */
+export class Exception extends Error {
+  readonly date: Date;
+
+  constructor(message: string, previous: Error, ...params: any[]) {
+    // Pass remaining arguments (including vendor specific ones) to parent constructor
+    super(message, { cause: previous, ...params });
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, Exception);
+    }
+
+    this.name = this.constructor.name;
+    // Custom debugging information
+    this.date = new Date();
+  }
+}
