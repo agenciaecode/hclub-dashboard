2d3854c0f045fcbd65c4e64d62a0c97b9ba55be2
extendslcc <lucas.lcc@hotmail.com>
2022-05-16 16:25:51 -0300

    feat(i18n): add more common default messages accordingly http status code

--- src/utils/i18n/error-messages.ts
+++ src/utils/i18n/error-messages.ts
@@ -6,6 +6,10 @@ export const errorMessageText = {
     'Falha ao tentar se comunicar com o servidor. Verifique sua conexão com a internet.',
   UNPREDICTED_ERROR:
     'Erro inesperado. Tente novamente mais tarde ou entre em contato com o suporte.',
+  [StatusCodes.UNAUTHORIZED]:
+    'Você não está autenticado para executar esta ação.',
+  [StatusCodes.FORBIDDEN]: 'Você não está autorizado a acessar este recurso.',
+  [StatusCodes.NOT_FOUND]: 'Recurso requisitado não encontrado.',
   [StatusCodes.TOO_MANY_REQUESTS]:
     'Você enviou muitas tentativas. Aguarde um momento.',
   CLIENT_ERROR_DEFAULT: 'Requisição inválida.',
