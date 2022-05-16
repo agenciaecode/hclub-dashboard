33b35f235e7f69aa3dbbd1d26df1db8485365320
extendslcc <lucas.lcc@hotmail.com>
2022-05-16 16:45:55 -0300

    refactor(exceptions): add common status code errors messages as default

--- src/services/http/HttpStatusCodeHandler.ts
+++ src/services/http/HttpStatusCodeHandler.ts
@@ -2,12 +2,27 @@ import { StatusCodes } from 'http-status-codes';
 
 import { ClientHttpException } from './HttpException';
 
+import { errorMessageText } from '@utils/i18n/error-messages';
+import { showToastErrorMessage } from '@/libs/toast/showToastMessage';
+
 type ExceptionHandlerCallback = (clientException: ClientHttpException) => void;
 
 type StatusCodeHandlersMap<ClientError extends ClientHttpException> = Partial<
   Record<StatusCodes, ExceptionHandlerCallback>
 > & {
   default?: (clientException: ClientError) => void;
+};
+
+const defaultStatusCodeHandler: StatusCodeHandlersMap<ClientHttpException> = {
+  [StatusCodes.UNAUTHORIZED]: () =>
+    showToastErrorMessage(errorMessageText[StatusCodes.UNAUTHORIZED]),
+  [StatusCodes.FORBIDDEN]: () =>
+    showToastErrorMessage(errorMessageText[StatusCodes.FORBIDDEN]),
+  [StatusCodes.NOT_FOUND]: () =>
+    showToastErrorMessage(errorMessageText[StatusCodes.NOT_FOUND]),
+  [StatusCodes.TOO_MANY_REQUESTS]: () =>
+    showToastErrorMessage(errorMessageText[StatusCodes.TOO_MANY_REQUESTS]),
+  default: () => showToastErrorMessage(errorMessageText.CLIENT_ERROR_DEFAULT),
 };
 
 /**
@@ -23,11 +38,15 @@ type StatusCodeHandlersMap<ClientError extends ClientHttpException> = Partial<
 export function handleClientExceptionByStatus<
   ClientError extends ClientHttpException,
 >(statusCodeHandlers: StatusCodeHandlersMap<ClientError>) {
+  const withDefaultHandlers = {
+    ...defaultStatusCodeHandler,
+    ...statusCodeHandlers,
+  };
   return (clientException: ClientError) => {
     const handler =
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      statusCodeHandlers[clientException.statusCode!] ||
-      statusCodeHandlers.default;
+      withDefaultHandlers[clientException.statusCode!] ||
+      withDefaultHandlers.default;
     if (handler) {
       handler(clientException);
       return;
