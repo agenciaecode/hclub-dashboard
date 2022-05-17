0366ea7f44565c459f0b3ec89d1f2afcb3897680
extendslcc <lucas.lcc@hotmail.com>
2022-05-17 12:02:42 -0300

    refactor(hook-form): add hook-form utility to set errors from ValidationHttpException

--- src/libs/hook-form/set-form-errors-from-validation-exception.ts
+++ src/libs/hook-form/set-form-errors-from-validation-exception.ts
@@ -2,7 +2,7 @@
 import { UseFormSetError } from 'react-hook-form';
 import { objectEntries } from '@antfu/utils';
 
-import { ValidationHttpException } from '@/services/http/HttpException';
+import { ValidationHttpException } from '@libs/http/HttpException';
 
 //! #TODO restrict arguments typing, instead of using any type
 export function setFormErrorsFromValidationException<
@@ -12,4 +12,18 @@ export function setFormErrorsFromValidationException<
   objectEntries(validationError.details).forEach(([key, value]) => {
     setError(key as string, { message: value?.join('\n') });
   });
+}
+
+//! #TODO restrict arguments typing, instead of using any type
+/**
+ * Create a function that will set hook-form errors from a validation exception
+ * @param setError - setError function from react-hook-form
+ */
+export function setFormErrorsFromException<
+  Error extends ValidationHttpException<any>,
+  Setter extends UseFormSetError<any>,
+>(setter: Setter) {
+  return (validationError: Error) => {
+    setFormErrorsFromValidationException(setter, validationError);
+  };
 }
