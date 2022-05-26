cc1bf15a64b97409394fc52e3749c74c292bdbee
extendslcc <lucas.lcc@hotmail.com>
2022-05-26 18:21:55 -0300

    refactor(LoginPage): add redirect to dashboard after successful login

--- src/features/auth/view/login/LoginPage.tsx
+++ src/features/auth/view/login/LoginPage.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import { useAuth } from '@libs/auth/react';
 
 import whiteWalletImage from '../../assets/images/wallet-white-revision-1-1.png';
 
@@ -11,27 +15,38 @@ import {
   StyledSplashSection,
 } from './LoginPage.styles';
 
-const LoginPage = () => (
-  <>
-    <Head>
-      <title>Login | HClub</title>
-      <meta name="description" content="H.club login" />
-    </Head>
-    <StyledMain>
-      <StyledContentSection>
-        <LoginForm />
-      </StyledContentSection>
-      <StyledSplashSection>
-        <Image
-          src={whiteWalletImage}
-          alt="Carteira Essencial Branca"
-          width={368}
-          height={560}
-        />
-      </StyledSplashSection>
-      <StyledFooter />
-    </StyledMain>
-  </>
-);
+const LoginPage = () => {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push('/dashboard');
+    }
+  }, [isAuthenticated, router]);
+
+  return (
+    <>
+      <Head>
+        <title>Login | HClub</title>
+        <meta name="description" content="H.club login" />
+      </Head>
+      <StyledMain>
+        <StyledContentSection>
+          <LoginForm />
+        </StyledContentSection>
+        <StyledSplashSection>
+          <Image
+            src={whiteWalletImage}
+            alt="Carteira Essencial Branca"
+            width={368}
+            height={560}
+          />
+        </StyledSplashSection>
+        <StyledFooter />
+      </StyledMain>
+    </>
+  );
+};
 
 export { LoginPage };
