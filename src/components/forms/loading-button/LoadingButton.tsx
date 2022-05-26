8fa76da3f61ba3be15ed8949ece1d1f0d495c774
extendslcc <lucas.lcc@hotmail.com>
2022-05-26 15:40:12 -0300

    fix(LoadingButton): fix wrong typing with forwardRef

--- src/components/forms/loading-button/LoadingButton.tsx
+++ src/components/forms/loading-button/LoadingButton.tsx
@@ -1,34 +1,29 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import { forwardRef } from 'react';
+import type { ElementRef, ComponentPropsWithoutRef } from 'react';
 
 import { CheckMarkSpinner } from '@components/feedback/checkmark-spinner';
 
 import { Button } from '../button';
 
-export type LoadingButtonProps = React.ComponentPropsWithoutRef<
-  typeof Button
-> & {
+export type LoadingButtonProps = ComponentPropsWithoutRef<typeof Button> & {
   isLoading: boolean;
   isSuccess: boolean;
 };
 
-const LoadingButton = React.forwardRef<
-  React.ComponentRef<typeof Button>,
+const LoadingButton = forwardRef<
+  ElementRef<typeof Button>,
   LoadingButtonProps
->(
-  (
-    { isLoading, isSuccess, children, ...buttonProps }: LoadingButtonProps,
-    ref,
-  ) => (
-    <Button disabled={isLoading} {...buttonProps} ref={ref}>
-      {isLoading || isSuccess ? (
-        <CheckMarkSpinner finished={isSuccess} />
-      ) : (
-        children
-      )}
-    </Button>
-  ),
-);
+  // eslint-disable-next-line react/prop-types
+>(({ isLoading, isSuccess, children, ...buttonProps }, forwardedRef) => (
+  <Button disabled={isLoading} {...buttonProps} ref={forwardedRef}>
+    {isLoading || isSuccess ? (
+      <CheckMarkSpinner finished={isSuccess} />
+    ) : (
+      children
+    )}
+  </Button>
+));
 
 LoadingButton.displayName = 'LoadingButton';
 
