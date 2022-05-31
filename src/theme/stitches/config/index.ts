3a133625e845923436713903ddd18fde5b7b3226
extendslcc <lucas.lcc@hotmail.com>
2022-05-31 17:52:06 -0300

    refactor(theme): create CSS type and reexport all theme

--- src/theme/stitches/config/index.ts
+++ src/theme/stitches/config/index.ts
@@ -1,10 +1,12 @@
-import { createStitches } from '@stitches/react';
+import { createStitches, CSS as StitchesCSS } from '@stitches/react';
 import mapValues from 'lodash.mapvalues';
 
 import { breakpoints } from '@/theme/styles/theme/default/breakpoints';
 import { utils } from '@/theme/styles/theme/default/utils';
 
 import { defaultTheme } from '../../styles/theme';
+
+export type CSS = StitchesCSS<typeof configTheme>;
 
 const { mobile, ...minBreakpoints } = breakpoints;
 
