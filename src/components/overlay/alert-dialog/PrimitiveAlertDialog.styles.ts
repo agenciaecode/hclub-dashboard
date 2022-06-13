b8004a7b9915b86cdc44962ad8c2b3ea6710c306
extendslcc <lucas.lcc@hotmail.com>
2022-06-13 18:19:04 -0300

    refactor(modal): add close animation to modal & alert

--- src/components/overlay/alert-dialog/PrimitiveAlertDialog.styles.ts
+++ src/components/overlay/alert-dialog/PrimitiveAlertDialog.styles.ts
@@ -7,9 +7,19 @@ const overlayShow = keyframes({
   '100%': { opacity: 1 },
 });
 
+const overlayHide = keyframes({
+  '0%': { opacity: 1 },
+  '100%': { opacity: 0 },
+});
+
 const contentShow = keyframes({
   '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.1)' },
   '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+});
+
+const contentHide = keyframes({
+  '0%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+  '100%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.1)' },
 });
 
 const StyledOverlay = styled(AlertDialogPrimitive.Overlay, {
@@ -18,6 +28,9 @@ const StyledOverlay = styled(AlertDialogPrimitive.Overlay, {
   inset: 0,
   '@media (prefers-reduced-motion: no-preference)': {
     animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1) forwards`,
+  },
+  '&[data-state="closed"]': {
+    animation: `${overlayHide} 150ms cubic-bezier(0.16, 1, 0.3, 1) forwards`,
   },
 });
 
@@ -36,6 +49,9 @@ const StyledAlertDialogContent = styled(AlertDialogPrimitive.Content, {
   padding: 25,
   '@media (prefers-reduced-motion: no-preference)': {
     animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  },
+  '&[data-state="closed"]': {
+    animation: `${contentHide} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
   },
   '&:focus': { outline: 'none' },
 });
