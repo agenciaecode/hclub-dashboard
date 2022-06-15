a6d0f72f5e18df9a02bf0fd0f7c79bad07d12bd8
extendslcc <lucas.lcc@hotmail.com>
2022-06-15 15:26:41 -0300

    refactor(exception): change default forbidden status error message

--- src/utils/i18n/error-messages.ts
+++ src/utils/i18n/error-messages.ts
@@ -8,7 +8,8 @@ export const errorMessageText = {
     'Erro inesperado. Tente novamente mais tarde ou entre em contato com o suporte.',
   [StatusCodes.UNAUTHORIZED]:
     'Você não está autenticado para executar esta ação.',
-  [StatusCodes.FORBIDDEN]: 'Você não está autorizado a acessar este recurso.',
+  [StatusCodes.FORBIDDEN]:
+    'Você não está autorizado a acessar este recurso, sua conta está bloqueada.',
   [StatusCodes.NOT_FOUND]: 'Recurso requisitado não encontrado.',
   [StatusCodes.TOO_MANY_REQUESTS]:
     'Você enviou muitas tentativas. Aguarde um momento.',
