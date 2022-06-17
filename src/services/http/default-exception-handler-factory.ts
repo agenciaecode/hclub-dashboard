768965acc9dcda51055905e738393c031ff56daf
extendslcc <lucas.lcc@hotmail.com>
2022-06-17 11:52:59 -0300

    refactor(exception): add default http status handler on http exception

--- src/services/http/default-exception-handler-factory.ts
+++ src/services/http/default-exception-handler-factory.ts
@@ -1,10 +1,12 @@
 import { HttpException } from '@libs/http/HttpException';
 import { fromHttpException } from '@libs/http/HttpExceptionHandler';
 import { showToastErrorMessage } from '@libs/toast/showToastMessage';
+import { handleClientExceptionByStatus } from '@services/http/default-status-code-handlers';
 import { errorMessageText } from '@utils/i18n/error-messages';
 
 const fromHttpExceptionWithDefaultHandlers = (exception: unknown) =>
   fromHttpException(exception as HttpException)
+    .setClientExceptionHandler(handleClientExceptionByStatus({}))
     .setServerExceptionHandler(() =>
       showToastErrorMessage(errorMessageText.SERVER_ERROR),
     )
