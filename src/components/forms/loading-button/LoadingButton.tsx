1eeeb1cb1045ed80ed2550bb4dafb17ee2276a84
extendslcc <lucas.lcc@hotmail.com>
2022-06-21 10:22:44 -0300

    refactor(LoadingButton): add animation end event prop

--- src/components/forms/loading-button/LoadingButton.tsx
+++ src/components/forms/loading-button/LoadingButton.tsx
@@ -1,4 +1,5 @@
-/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-props-no-spreading,react/prop-types */
+
 import { forwardRef } from 'react';
 import type { ElementRef, ComponentPropsWithoutRef } from 'react';
 
@@ -9,22 +10,31 @@ import { Button } from '../button';
 export type LoadingButtonProps = ComponentPropsWithoutRef<typeof Button> & {
   isLoading: boolean;
   isSuccess: boolean;
+  onAnimationFinished?: () => void;
 };
 
-const LoadingButton = forwardRef<
-  ElementRef<typeof Button>,
-  LoadingButtonProps
-  // eslint-disable-next-line react/prop-types
->(({ isLoading, isSuccess, children, ...buttonProps }, forwardedRef) => (
-  <Button disabled={isLoading} {...buttonProps} ref={forwardedRef}>
-    {isLoading || isSuccess ? (
-      <CheckMarkSpinner finished={isSuccess} />
-    ) : (
-      children
-    )}
-  </Button>
-));
+const LoadingButton = forwardRef<ElementRef<typeof Button>, LoadingButtonProps>(
+  (
+    { isLoading, isSuccess, onAnimationFinished, children, ...buttonProps },
+    forwardedRef,
+  ) => (
+    <Button disabled={isLoading} {...buttonProps} ref={forwardedRef}>
+      {isLoading || isSuccess ? (
+        <CheckMarkSpinner
+          finished={isSuccess}
+          onAnimationFinish={onAnimationFinished}
+        />
+      ) : (
+        children
+      )}
+    </Button>
+  ),
+);
 
 LoadingButton.displayName = 'LoadingButton';
+
+LoadingButton.defaultProps = {
+  onAnimationFinished: undefined,
+};
 
 export { LoadingButton };
