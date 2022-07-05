aab0d04ba534ebe37ec943739b98f03a11cbee64
extendslcc <lucas.lcc@hotmail.com>
2022-07-05 16:18:50 -0300

    refactor(CardAvatar): decouple form into separate component to lazy load stateful logic

--- src/features/cards/view/edit-card/sections/card-avatar/CardAvatar.tsx
+++ src/features/cards/view/edit-card/sections/card-avatar/CardAvatar.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading,import/no-cycle */
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React, { useEffect, useRef } from 'react';
+import React, { memo, useEffect, useRef } from 'react';
 
 import { VisuallyHidden } from '@components/disclosure/visually-hidden';
 import { Spinner } from '@components/feedback/spinner';
@@ -14,13 +13,14 @@ import { showToastSuccessMessage } from '@libs/toast/showToastMessage';
 import { useHttpExceptionHandler } from '@services/http/hooks/useHttpExceptionHandler';
 import { animationDelay } from '@utils/animation/animation-delay';
 
-import type { CardType } from '@features/cards';
+import { CardType } from '@features/cards';
 import { useUserProfileQuery } from '@features/user';
 
 import defaultAvatar from '@assets/images/user-avatar.svg';
 
 import { useShowCardQuery } from '../../api/showCard';
 import { SectionWrapper } from '../../components/section-wrapper';
+import { useCardSlug } from '../../hooks/useCardSlug';
 import {
   SetCardAvatarValidationError,
   useCardAvatarMutation,
@@ -33,68 +33,17 @@ import {
   StyledFlexRow,
 } from './CardAvatar.styles';
 
-type CardAvatarProps = {
-  cardSlug: CardType;
-};
-
 function getFirstFileFromFileList(fileList: FileList) {
   const [avatarFile] = Array.from(fileList);
   return avatarFile;
 }
 
-export const CardAvatar = ({ cardSlug }: CardAvatarProps) => {
-  const [previewUrl, setPreviewUrl] = React.useState<string | null>(null);
-  const avatarFileInputRef = useRef<HTMLInputElement | null>(null);
+export const CardAvatar = () => {
+  const cardSlug = useCardSlug();
   const userProfileQuery = useUserProfileQuery();
   const showCardQuery = useShowCardQuery({
     card: cardSlug,
   });
-  const cardAvatarMutation = useCardAvatarMutation();
-  const {
-    formState: { errors: setCardAvatarFormErrors },
-    ...setCardAvatarForm
-  } = useFormWithSchema(setCardAvatarSchema);
-  const { ref: avatarFormRef, ...avatarFormRegister } =
-    setCardAvatarForm.register('avatar');
-  const avatarFormValue = setCardAvatarForm.watch('avatar');
-
-  useHttpExceptionHandler(cardAvatarMutation.error, exceptionHandler =>
-    exceptionHandler
-      .setValidationExceptionHandler<SetCardAvatarValidationError>(
-        setFormErrorsFromException(setCardAvatarForm.setError),
-      )
-      .executeHandler(),
-  );
-
-  useEffect(() => {
-    if (!avatarFormValue) return undefined;
-    const selectedFile = getFirstFileFromFileList(avatarFormValue);
-    if (!selectedFile) {
-      setPreviewUrl(null);
-      return undefined;
-    }
-    const selectedFileGlobUrl = URL.createObjectURL(selectedFile);
-    setPreviewUrl(selectedFileGlobUrl);
-
-    return () => URL.revokeObjectURL(selectedFileGlobUrl);
-  }, [avatarFormValue]);
-
-  const handleSetAvatarFormSubmit = setCardAvatarForm.handleSubmit(
-    submittedFormValues => {
-      if (cardAvatarMutation.isLoading) return;
-      cardAvatarMutation.mutate(
-        {
-          avatar: getFirstFileFromFileList(submittedFormValues.avatar),
-          card: cardSlug,
-        },
-        {
-          onSuccess: () => {
-            showToastSuccessMessage('Avatar atualizado com sucesso!');
-          },
-        },
-      );
-    },
-  );
 
   return (
     <SectionWrapper title="Foto de Perfil">
@@ -119,85 +68,157 @@ export const CardAvatar = ({ cardSlug }: CardAvatarProps) => {
           title="Foto de perfil"
           description="Selecione uma imagem para adicionar ou alterar seu avatar"
         >
-          <form onSubmit={handleSetAvatarFormSubmit}>
-            <StyledFlexRow>
-              <StyledFigureContainer>
-                {previewUrl && (
-                  <Image src={previewUrl} layout="fill" alt="avatar" />
-                )}
-                {!previewUrl &&
-                  (showCardQuery.isSuccess ? (
-                    <Image
-                      src={
-                        showCardQuery.data.avatar?.url ??
-                        userProfileQuery.data?.avatar?.url ??
-                        defaultAvatar
-                      }
-                      layout="fill"
-                      alt="avatar"
-                    />
-                  ) : (
-                    <Spinner color="secondary" css={{ margin: '5rem 5rem' }} />
-                  ))}
-              </StyledFigureContainer>
-              <Button
-                btn="secondary"
-                type="button"
-                css={{
-                  marginRight: 'none',
-                  '@sm': {
-                    marginRight: 'auto',
-                  },
-                }}
-                onClick={() => avatarFileInputRef.current?.click()}
-              >
-                Procurar imagem
-              </Button>
-              <VisuallyHidden>
-                <input
-                  type="file"
-                  tabIndex={-1}
-                  {...avatarFormRegister}
-                  ref={ref => {
-                    avatarFormRef(ref);
-                    avatarFileInputRef.current = ref;
-                  }}
-                />
-              </VisuallyHidden>
-            </StyledFlexRow>
-            <ErrorLabel
-              errorMessage={setCardAvatarFormErrors.avatar?.message as string}
-              css={{ marginTop: '1rem' }}
-            />
-            <StyledButtonsWrapper>
-              <DialogClose asChild>
-                <Button
-                  btn="secondary"
-                  type="reset"
-                  onClick={() => {
-                    setCardAvatarForm.reset();
-                    setPreviewUrl(null);
-                  }}
-                >
-                  Cancelar
-                </Button>
-              </DialogClose>
-              <LoadingButton
-                isLoading={cardAvatarMutation.isLoading}
-                isSuccess={cardAvatarMutation.isSuccess}
-                onAnimationFinished={async () => {
-                  await animationDelay();
-                  setCardAvatarForm.reset();
-                  cardAvatarMutation.reset();
-                  setPreviewUrl(null);
-                }}
-              >
-                Salvar
-              </LoadingButton>
-            </StyledButtonsWrapper>
-          </form>
+          <CardAvatarForm
+            cardSlug={cardSlug}
+            showCardQuery={showCardQuery}
+            userProfileQuery={userProfileQuery}
+          />
         </DescriptiveModal>
       </StyledContentWrapper>
     </SectionWrapper>
   );
 };
+
+type CardAvatarFormProps = {
+  showCardQuery: ReturnType<typeof useShowCardQuery>;
+  userProfileQuery: ReturnType<typeof useUserProfileQuery>;
+  cardSlug: CardType;
+};
+
+/**
+ * Decouple modal form stateful logic from the component.
+ */
+const CardAvatarForm = memo(
+  ({ showCardQuery, userProfileQuery, cardSlug }: CardAvatarFormProps) => {
+    const [previewUrl, setPreviewUrl] = React.useState<string | null>(null);
+    const avatarFileInputRef = useRef<HTMLInputElement | null>(null);
+    const cardAvatarMutation = useCardAvatarMutation();
+    const {
+      formState: { errors: setCardAvatarFormErrors },
+      ...setCardAvatarForm
+    } = useFormWithSchema(setCardAvatarSchema);
+    const { ref: avatarFormRef, ...avatarFormRegister } =
+      setCardAvatarForm.register('avatar');
+    const avatarFormValue = setCardAvatarForm.watch('avatar');
+
+    useHttpExceptionHandler(cardAvatarMutation.error, exceptionHandler =>
+      exceptionHandler
+        .setValidationExceptionHandler<SetCardAvatarValidationError>(
+          setFormErrorsFromException(setCardAvatarForm.setError),
+        )
+        .executeHandler(),
+    );
+
+    useEffect(() => {
+      if (!avatarFormValue) return undefined;
+      const selectedFile = getFirstFileFromFileList(avatarFormValue);
+      if (!selectedFile) {
+        setPreviewUrl(null);
+        return undefined;
+      }
+      const selectedFileGlobUrl = URL.createObjectURL(selectedFile);
+      setPreviewUrl(selectedFileGlobUrl);
+
+      return () => URL.revokeObjectURL(selectedFileGlobUrl);
+    }, [avatarFormValue]);
+
+    const handleSetAvatarFormSubmit = setCardAvatarForm.handleSubmit(
+      submittedFormValues => {
+        if (cardAvatarMutation.isLoading) return;
+        cardAvatarMutation.mutate(
+          {
+            avatar: getFirstFileFromFileList(submittedFormValues.avatar),
+            card: cardSlug,
+          },
+          {
+            onSuccess: () => {
+              showToastSuccessMessage('Avatar atualizado com sucesso!');
+            },
+          },
+        );
+      },
+    );
+
+    return (
+      <form onSubmit={handleSetAvatarFormSubmit}>
+        <StyledFlexRow>
+          <StyledFigureContainer>
+            {previewUrl && (
+              <Image src={previewUrl} layout="fill" alt="avatar" />
+            )}
+            {!previewUrl &&
+              (showCardQuery.isSuccess ? (
+                <Image
+                  src={
+                    showCardQuery.data.avatar?.url ??
+                    userProfileQuery.data?.avatar?.url ??
+                    defaultAvatar
+                  }
+                  layout="fill"
+                  alt="avatar"
+                />
+              ) : (
+                <Spinner color="secondary" css={{ margin: '5rem 5rem' }} />
+              ))}
+          </StyledFigureContainer>
+          <Button
+            btn="secondary"
+            type="button"
+            css={{
+              marginRight: 'none',
+              '@sm': {
+                marginRight: 'auto',
+              },
+            }}
+            onClick={() => avatarFileInputRef.current?.click()}
+          >
+            Procurar imagem
+          </Button>
+          <VisuallyHidden>
+            <input
+              type="file"
+              tabIndex={-1}
+              {...avatarFormRegister}
+              ref={ref => {
+                avatarFormRef(ref);
+                avatarFileInputRef.current = ref;
+              }}
+            />
+          </VisuallyHidden>
+        </StyledFlexRow>
+        <ErrorLabel
+          errorMessage={setCardAvatarFormErrors.avatar?.message as string}
+          css={{ marginTop: '1rem' }}
+        />
+        <StyledButtonsWrapper>
+          <DialogClose asChild>
+            <Button
+              btn="secondary"
+              type="reset"
+              onClick={() => {
+                setCardAvatarForm.reset();
+                setPreviewUrl(null);
+              }}
+            >
+              Cancelar
+            </Button>
+          </DialogClose>
+          <LoadingButton
+            isLoading={cardAvatarMutation.isLoading}
+            isSuccess={cardAvatarMutation.isSuccess}
+            onAnimationFinished={async () => {
+              await animationDelay();
+              setCardAvatarForm.reset();
+              cardAvatarMutation.reset();
+              setPreviewUrl(null);
+            }}
+          >
+            Salvar
+          </LoadingButton>
+        </StyledButtonsWrapper>
+      </form>
+    );
+  },
+);
+
+CardAvatarForm.displayName = 'CardAvatarForm';
