799ddee32cca62d62ff12f0c520cc8249556b98f
extendslcc <lucas.lcc@hotmail.com>
2022-07-05 11:10:47 -0300

    feat(components): add Text generic utility component

---
+++ src/components/typography/text/Text.tsx
@@ -0,0 +1,44 @@
+import { styled } from '@/theme';
+
+export const Text = styled('span', {
+  variants: {
+    color: {
+      primary: {
+        color: '$textBlack',
+      },
+      secondary: {
+        color: '$textWhite',
+      },
+    },
+    size: {
+      sm: {
+        fontSize: '$sm',
+        lineHeight: '$sm',
+      },
+      base: {
+        fontSize: '$base',
+        lineHeight: '$base',
+      },
+      lg: {
+        fontSize: '$lg',
+        lineHeight: '$lg',
+      },
+      xl: {
+        fontSize: '$xl',
+        lineHeight: '$xl',
+      },
+      '2xl': {
+        fontSize: '$2xl',
+        lineHeight: '$2xl',
+      },
+      '3xl': {
+        fontSize: '$3xl',
+        lineHeight: '$3xl',
+      },
+    },
+  },
+  defaultVariants: {
+    color: 'primary',
+    size: 'base',
+  },
+});
