cf1faca5ae76e0c4219b676f805582f3eadb4658
Rafael Capoci <rafaelcapoci@gmail.com>
2022-07-08 16:25:04 -0300

    fix(component): correcao de ref do input field file e upload de arquivos

--- src/features/hdrive/view/hdrive/components/navbar-explorer/NavbarExplorer.tsx
+++ src/features/hdrive/view/hdrive/components/navbar-explorer/NavbarExplorer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useRef } from 'react';
 
 import { FieldValues, useForm } from 'react-hook-form';
@@ -12,12 +13,16 @@ import {
 } from './NavbarExplorer.styles';
 
 const NavbarExplorer = () => {
-  const inputFileRef = useRef();
-  const { handleSubmit } = useForm();
+  const inputFileRef = useRef<HTMLInputElement | null>(null);
+  const { handleSubmit, register } = useForm();
 
   async function onSubmit(bodyRequest: FieldValues) {
-    const endpoint = '/files/';
-    await apiDashboard.post(endpoint, bodyRequest);
+    const formData = new FormData();
+    formData.append('file', bodyRequest.target.files[0]);
+
+    const endpoint = 'hdrive/files/';
+
+    await apiDashboard.post(endpoint, formData);
   }
 
   return (
@@ -25,10 +30,19 @@ const NavbarExplorer = () => {
       <StyledLeftContent> </StyledLeftContent>
       <StyledRightContent>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input hidden type="file" ref={inputFileRef} />
+          <input
+            hidden
+            type="file"
+            {...register('file')}
+            ref={originalRef => {
+              register('file').ref(originalRef);
+              inputFileRef.current = originalRef;
+            }}
+            onChange={onSubmit}
+          />
           <Button
             btn="secondary"
-            onClick={() => inputFileRef.current.click()}
+            onClick={() => inputFileRef?.current?.click()}
             outlined
             type="button"
           >
