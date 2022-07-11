7f7382dbb892fc4fd3f5ec329be0dda4ecbe8b6f
extendslcc <lucas.lcc@hotmail.com>
2022-07-11 15:02:07 -0300

    refactor(Select): allow react node to be passed as text prop of SelectOption

--- src/components/forms/select/Select.tsx
+++ src/components/forms/select/Select.tsx
@@ -1,5 +1,10 @@
 /* eslint-disable react/prop-types,prefer-arrow-callback,react/jsx-props-no-spreading */
-import React, { ComponentProps, ElementRef, forwardRef } from 'react';
+import React, {
+  ComponentProps,
+  ElementRef,
+  forwardRef,
+  ReactNode,
+} from 'react';
 
 import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
 import { SelectItemProps } from '@radix-ui/react-select';
@@ -23,16 +28,18 @@ export type SelectProps = SelectPrimitiveProps &
     label: string;
   };
 
-export type SelectOptionProps = SelectItemProps & { text: string };
+// eslint-disable-next-line react/require-default-props
+export type SelectOptionProps = SelectItemProps & { text?: ReactNode };
 
 export const SelectOption = ({
   text,
   disabled,
   value,
   textValue,
+  children,
 }: SelectOptionProps) => (
   <SelectItem value={value} disabled={disabled} textValue={textValue}>
-    <SelectItemText>{text}</SelectItemText>
+    <SelectItemText>{text || children}</SelectItemText>
   </SelectItem>
 );
 
--- src/components/forms/select/index.tsx
+++ src/components/forms/select/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/export */
+export * from './PrimitiveSelect';
 export { Select, SelectOption } from './Select';
 export type { SelectProps, SelectOptionProps } from './Select';
 export { ControlledSelect } from './templates/controlled-select';
